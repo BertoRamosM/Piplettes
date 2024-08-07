9f52f12cab62ed752914465bf9b272147d5f04bc
ARamos <albertramos902@gmail.com>
2024-08-07 16:09:11 +0000

    trying styles in hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,46 @@
-import React from 'react'
-import Button from '../Button'
+import React from "react";
+import Button from "../Button";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="dark:text-gray-800">
-      <div className="container mx-auto flex flex-col items-pretty px-4 text-pretty md:px-10 lg:px-32 xl:max-w-3xl pb-20">
+    <section className="">
+      <div className="container mx-auto flex flex-col px-4 text-pretty md:px-10 lg:px-32 pb-24">
         <h1 className="text-4xl font-bold leading-none sm:text-5xl">
-          Quisquam necessita vel
-          <span className="dark:text-magenta-600">laborum doloribus</span>
-          delectus
+          <span className="dark:text-magenta-600">Les Piplettes </span>
+          de Granville
         </h1>
-        <p className="px-8 mt-8 mb-12 text-lg">
-          Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab
-          amet vero eaque explicabo!
-        </p>
+        <h2 className="text-xl font-bold leading-none pl-1 pt-2">
+          Explorez,
+          <span className="text-orangy-600"> Découvrez</span>, Débattez
+        </h2>
+        <div className="flex flex-col  items-center justify-center gap-2 relative">
+          <p className="mt-8 mb-12 text-lg">
+            Situés à Granville (50400), nous, Les Piplettes de Granville, nous
+            spécialisons dans les arts et la culture, avec un accent particulier
+            sur le cinéma. Engagez-vous dans nos Ciné Débats pour des
+            discussions enrichissantes et stimulantes après chaque projection.
+          </p>
+          <div className="relative w-48 h-48 mb-8">
+            <Image
+              src="/illustration.png"
+              alt="Les Piplettes logo"
+              layout="responsive"
+              width={192}
+              height={192}
+              className="object-contain"
+            />
+          </div>
+        </div>
         <div className="flex flex-wrap justify-center">
-          <Button text={"Get Started"}></Button>
-           
+          <Button text={"Nos prochains événements"}></Button>
           <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-900 dark:border-gray-300 cursor-pointer">
-            Learn more
+            À propos de nous
           </button>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default Hero
+export default Hero;
