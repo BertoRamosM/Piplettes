d8d2e967697242ba8fb8c40f3d6bae6d93324977
ARamos <albertramos902@gmail.com>
2024-08-08 16:21:43 +0000

    styiling single page for movies

--- src/app/events/[id]/page.jsx
+++ src/app/events/[id]/page.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import Movies from "../../../utils/mockMovies.json";
+import Image from 'next/image';
 
 
 const EventPage = ({params}) => {
-  console.log(params)
 
   const movie = Movies.find((movie) => movie.id === params.id);
 
 
-console.log(movie)
   return (
     <div className="">
-      <h1>{movie.title}</h1>
-      <h3>{movie.director}</h3>
-      <p>{movie.synopsis}</p>
+      <div className="flex gap-4 items-center justify-between pb-8">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-5xl">{movie.title}</h1>
+          <h3 className="text-xl">
+            {movie.director}, <span className='text-sm'>{movie.release}</span>
+          </h3>
+
+          <div className="flex gap-2">
+            {movie.themes.map((theme, index) => (
+              <div
+                className="border-2 border-zinc-400 p-1 rounded-xl"
+                key={index}
+              >
+                {theme}
+              </div>
+            ))}
+          </div>
+        </div>
+        <Image src={movie.image} alt={movie.title} width={300} height={200} />
+      </div>
+      <h4 className="text-2xl text-bold underline pb-2">Synopsis</h4>
+      <p className="text-pretty">{movie.synopsis}</p>
     </div>
   );
 }
