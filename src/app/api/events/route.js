45230cf4d34096e5cd001f1c34947478743ac772
ARamos <albertramos902@gmail.com>
2024-09-09 15:51:35 +0000

    events not fetching

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -2,14 +2,13 @@ import { NextResponse } from "next/server";
 import connect from "../../../utils/db";
 import Events from "../../../models/Events.js";
 
-export async function GET() {
+export const GET = async (request) => {
   try {
     await connect();
 
     const events = await Events.find().sort({ date: 1 });
     console.log("Fetched events:", events);
-      return NextResponse.json(events, { status: 200 });
-
+    return new NextResponse(JSON.stringify(events), { status: 200 });
   } catch (error) {
     console.error("Error fetching events:", error.stack); // Log the full error stack
     return new NextResponse("Database error!", { status: 500 });
