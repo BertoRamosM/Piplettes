817029f0b9e861e1c5fdab5b3eacb967f8df7c56
ARamos <albertramos902@gmail.com>
2024-09-09 11:22:39 +0000

    testing github errors

--- piplettes/src/app/sitemap.ts
+++ piplettes/src/app/sitemap.ts
@@ -1,22 +1,37 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // Fetch Events
   const resEvents = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/events`
   );
-  const events = await resEvents.json();
+
+  let events = [];
+  if (resEvents.ok) {
+    events = await resEvents.json();
+  } else {
+    console.error(`Failed to fetch events: ${resEvents.statusText}`);
+  }
 
   const eventsEntries: MetadataRoute.Sitemap = events.map(({ _id }) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/events/${_id}`,
   }));
 
+  // Fetch Blogs (assuming this is a different endpoint)
   const resBlog = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs`);
-  const blogs = await resBlog.json();
+
+  let blogs = [];
+  if (resBlog.ok) {
+    blogs = await resBlog.json();
+  } else {
+    console.error(`Failed to fetch blogs: ${resBlog.statusText}`);
+  }
 
   const blogEntries: MetadataRoute.Sitemap = blogs.map(({ _id }) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/events/${_id}`,
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${_id}`,
   }));
 
+  // Return the complete sitemap
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
@@ -29,9 +44,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog`,
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
