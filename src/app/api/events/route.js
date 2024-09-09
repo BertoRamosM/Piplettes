e2e01095a1d7042603c2759e04319e4a3577880c
ARamos <albertramos902@gmail.com>
2024-09-09 15:59:56 +0000

    events not fetching

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -14,36 +14,3 @@ export const GET = async (request) => {
     return new NextResponse("Database error!", { status: 500 });
   }
 };
-
-export const POST = async (request) => {
-  try {
-    await connect();
-
-    const body = await request.json();
-    console.log("Request body:", body); // Log the request body for debugging
-
-    const newEvent = new Events({
-      title: body.title,
-      director: body.director,
-      desc: body.desc,
-      synopsis: body.synopsis,
-      image: body.image,
-      date: body.date || null,
-      hour: body.hour || "",
-      day: body.day || "",
-      month: body.month || "",
-      themes: body.themes || [],
-      release: body.release,
-    });
-
-    await newEvent.save();
-    console.log("Event created:", newEvent); // Log the created event
-
-    return new NextResponse("Event created successfully", { status: 201 });
-  } catch (error) {
-    console.error("Error creating event:", error.stack); // Log the full error stack
-    return new NextResponse(`Failed to create event: ${error.message}`, {
-      status: 500,
-    });
-  }
-};
